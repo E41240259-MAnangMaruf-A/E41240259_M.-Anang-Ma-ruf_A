4a3c6e84682388da22a50fb0983ee3cb0fc8e3c3
E41240259-MAnangMaruf-A <ananghyeong@gmail.com>
2025-02-10 20:47:59 +0700

    Create Minggu2.java

---
+++ E41240259_M. ANANG MA'RUF_A_MINGGU 2/Minggu2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minggu2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Minggu2 {
+
+    /**
+     * @param args the command line arguments
+     */
+        public static void main(String[] args) {
+        // Deklarasi variabel
+        String fName = "Lisa";        // Nama depan
+        String IName = "Palombo";     // Nama belakang
+        String stuId = "123456789";   // ID siswa
+        String stuStatus = "Active";  // Status siswa
+
+        // Menampilkan informasi siswa
+        String StudentName = fName + " " + IName; 
+        System.out.println("===========================================");
+        System.out.println("            STUDENT INFORMATIONS         ");
+        System.out.println("===========================================");
+        System.out.println("Student Name   : " + StudentName);
+        System.out.println("Student ID     : " + stuId);
+        System.out.println("Student Status : " + stuStatus);
+        System.out.println("===========================================");
+    }
+}
